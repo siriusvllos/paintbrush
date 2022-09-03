3c6befe4a2095de3ee63e426b889cdf78d7640e1
sirius <siriusvllos@gmail.com>
2022-09-03 15:07:46 -0300

    primeira parte do issue #17 mas ainda sem a funcao pronta

---
+++ paintbrush.js
@@ -0,0 +1,21 @@
+// funcoes p escolher cor de fundo e cor do pixel
+
+var corEscolhida = undefined;
+function essaCor(cor) {
+      corEscolhida = cor;
+}
+function mudarCor(clickedPixel) {
+     clickedPixel.style.backgroundColor = corEscolhida;
+}
+function limpar() {
+      const pixel = document.getElementsByName ("one pixel");
+      for (i = 0; i <= pixel.length; i = i + 1) { 
+            pixel[i].style.backgroundColor = corEscolhida;
+      }
+}
+
+// funcoes p mudar o tema
+
+function mudarTema() {
+    
+}
